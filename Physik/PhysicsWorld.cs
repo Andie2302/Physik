f65713411d84bf995d73228a26369519102476e2
andreas <Andreas.Zwander@gmail.com>
2026-01-31 13:35:24 +0100

    Remove redundant comment in `PhysicsWorld` class to improve code readability.

--- Physik/PhysicsWorld.cs
+++ Physik/PhysicsWorld.cs
@@ -5,5 +5,4 @@ namespace Physik;
 public class PhysicsWorld
 {
     public Vector3 Gravity { get; set; } = new Vector3(0, -9.81f, 0);
-    // Hier könnten später auch Wind oder Luftwiderstand stehen
 }
