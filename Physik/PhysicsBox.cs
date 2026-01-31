3bd06ff77406f36db513124eb9cd25ee1a0669cb
andreas <Andreas.Zwander@gmail.com>
2026-01-31 14:36:49 +0100

    Add physical properties to `PhysicsBox` for mass, drag, and area calculations

--- Physik/PhysicsBox.cs
+++ Physik/PhysicsBox.cs
@@ -6,11 +6,22 @@ public abstract class PhysicsBox
 {
     public Vector3 Position; 
     public Vector3 Size;
-
     public Vector3 Velocity;
 
+    // Physikalische Basiseigenschaften
+    public float Mass { get; init; } = 1.0f;
+    public bool IsStatic { get; set; } 
+    public float DragCoefficient { get; set; } = 1.05f; // Standardwert für Boxen
+
+    // Die "Faulheit" (1/m)
+    public float InverseMass => IsStatic || Mass <= 0 ? 0f : 1.0f / Mass;
+
+    // Hilfsgrößen für die Kollision
     public Vector3 Min => Position;
     public Vector3 Max => Position + Size;
-
-    public bool IsStatic { get; set; } 
+    
+    // Flächen (Wichtig für Wind!)
+    public float AreaX => Size.Y * Size.Z; // Seite
+    public float AreaY => Size.X * Size.Z; // Oben/Unten
+    public float AreaZ => Size.X * Size.Y; // Vorne/Hinten
 }
