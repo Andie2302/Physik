2f725b41dc3605da23663065685510994eff5af0
andreas <Andreas.Zwander@gmail.com>
2026-01-31 13:35:17 +0100

    Remove comments and unused descriptions in `PhysicsBox` class to enhance code clarity.

--- Physik/PhysicsBox.cs
+++ Physik/PhysicsBox.cs
@@ -4,17 +4,13 @@ namespace Physik;
 
 public class PhysicsBox
 {
-    // Position und Ausdehnung (AABB)
     public Vector3 Position; 
     public Vector3 Size;
-    
-    // Bewegung
+
     public Vector3 Velocity;
-    
-    // Hilfseigenschaften für die Berechnung
+
     public Vector3 Min => Position;
     public Vector3 Max => Position + Size;
 
-    // Optionale Logik: Ist das Objekt fest (Boden) oder beweglich (Spieler)?
     public bool IsStatic { get; set; } 
 }
